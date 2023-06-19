6f80f0df9f19d54177ac35cec4cf3dba62c9de59
rumeysaai <rumeysaakgunn@gmail.com>
2023-06-20 01:13:08 +0300

    syntax fixed

--- CRM-Backend/CRM-DataAccess/Entities/Sector.cs
+++ CRM-Backend/CRM-DataAccess/Entities/Sector.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CRM_DataAccess.Entities
 {
-    public class Sektor
+    public class Sector
     {
         public int Id { get; set; }
         public string Name { get; set; }
