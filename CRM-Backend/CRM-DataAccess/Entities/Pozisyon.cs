4f7f12fb07662a7b664308136fe06e1fe1fec0b2
rumeysaai <rumeysaakgunn@gmail.com>
2023-06-19 19:35:26 +0300

    DbContext setup

---
+++ CRM-Backend/CRM-DataAccess/Entities/Pozisyon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_Data.Entities
+{
+    public class Pozisyon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+}
